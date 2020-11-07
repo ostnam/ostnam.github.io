9055e4a594f47ad5bc616af5c266a0f75b20cf30
ostnam <tsougaevm@gmail.com>
2020-11-07 12:35:19 +0100

    added win conditions, it's now functional as a regular C4 game

--- mc4/script.js
+++ mc4/script.js
@@ -6,6 +6,8 @@ let gameframe = document.getElementById("gameframe")
 let gamearray = []
 let currentPlay = 1
 let floor = [0, 0, 0, 0, 0, 0, 0]
+let minscope = 0
+let maxscope = 0
 
 function createrow() { // this function creates a single row in the DOM
     let row = document.createElement("div") // row is a div with the id row
@@ -26,7 +28,7 @@ function createrow() { // this function creates a single row in the DOM
         currentrow.append(cell)
     }
     rowcount++
-    gamearray.push([null, null, null, null, null, null, null])
+    gamearray.push(["x", "x", "x", "x", "x", "x", "x"])
 }
 
 
@@ -44,10 +46,11 @@ function playerinput(id) {
     // let playedRow = newId.slice(0, -1)
     let finalRow = floor[playedColumn]
     floor[playedColumn]++
-    gamearray[finalRow][playedColumn] = currentPlay % 2
+    gamearray[finalRow][playedColumn] = currentPlay % 2 + ""
     let playedCellId = "cell" + finalRow + playedColumn 
     let playedCell = document.getElementById(playedCellId)
     playedCell.setAttribute("class", "cellplay" + (currentPlay % 2))
+    checkwin(finalRow, playedColumn)
     currentPlay++
 }
 
@@ -74,6 +77,67 @@ function unhover(id) {
     let hoveredCellId = "cell" + finalRow + hoveredColumn
     let hoveredCell = document.getElementById(hoveredCellId)
     hoveredCell.setAttribute("class", "cell")
+}
+
+
+function checkwin(finalRow, cellColumn){
+    let playedrow = gamearray[finalRow]
+    let rowString = playedrow.join("")
+    if (rowString.includes("1111") || rowString.includes("0000")) {
+        victory(currentPlay % 2)
+    }
+    if (finalRow >=3) {
+        let playedColumn = [gamearray[finalRow-3][cellColumn],
+                            gamearray[finalRow-2][cellColumn],
+                            gamearray[finalRow-1][cellColumn],
+                            gamearray[finalRow][cellColumn]]
+        let columnString = playedColumn.join("")
+        if (columnString.includes("1111") || columnString.includes("0000")) {
+            victory(currentPlay % 2)
+        }
+    }
+
+    let workingDiag = []
+    let diagString = ""
+    for (i = -3; i <4; i++) { // we will take the value of cells with a maximum offset of 3 from the current cell
+        let workingRowNumber = finalRow + i
+        if (workingRowNumber >= 0 && workingRowNumber < gamearray.length){ // checking that the  working row exists
+            let workingColumnNumber = parseInt(cellColumn) + i
+            if (0 <= workingColumnNumber && workingColumnNumber <= 6) {
+                let workingCell = gamearray[workingRowNumber][workingColumnNumber]
+                workingDiag.push(workingCell)            
+            }
+        }
+    }
+    diagString += workingDiag.join("")
+
+    let workingDiag2 = []
+    for (i = -3; i <4; i++) { // we will take the value of cells with a maximum offset of 3 from the current cell
+        let workingRowNumber = finalRow + i
+        if (workingRowNumber >= 0 && workingRowNumber < gamearray.length){ // checking that the  working row exists
+            let workingColumnNumber = parseInt(cellColumn) - i
+            if (0 <= workingColumnNumber && workingColumnNumber <= 6) {
+                let workingCell = gamearray[workingRowNumber][workingColumnNumber]
+                workingDiag2.push(workingCell)            
+               }
+            }
+        }
+    
+
+    diagString += ","
+    diagString += workingDiag2.join("")
+    console.log(diagString)
+    if (diagString.includes("1111") || diagString.includes("0000")) {
+        victory(currentPlay % 2)}
+    }
+
+
+function victory(player) {
+    if (player === 1){
+        alert("Orange won!")}
+    else {
+        alert("Purple won!")
+    }
 }
 
 if (INIT === false) {

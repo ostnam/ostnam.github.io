b9af3127619b08c762de3c12778d30d837e8bdbb
ostnam <tsougaevm@gmail.com>
2020-11-14 17:18:00 +0100

    added list page and other improvements

--- UFC/script.js
+++ UFC/script.js
@@ -7,22 +7,31 @@ const DEDscreen = () => {
     // the display is stored in an array of 5 strings (one for each row), there are 24 cells per row
     
     let planeData = {
-        COM1: 1,
-        COM2: 6,
+        COM1: 292.30,
+        COM2: 5,
         currentSTPT: 1,
-        STPTMode: " ",
-        IFFModeStr: " 12 4",
+        STPTMode: 0,
         IFFCode: 7500,
-        NavMode: "T",
-        currentTACAN: "75X",
-        timeStr: "00:00:00",
-        COM1str: "1     ",
-        COM2str: "6     ",
+        currentTACAN: 75,
+        currentTACANMode: 0,
         blink: 0,
         currentDEDPage: "main",
         currentDEDtab: 0,
         currentRockerLocation: 0,
-    };
+        COM1mode: "man",
+        COM2mode: "preset",
+    }; // this object stores various data
+
+    let planeDataStr = {
+        STPTMode: " ",
+        IFFModeStr: " 12 4",
+        NavMode: "T",
+        currentTACAN: "75X",
+        timeStr: "00:00:00",
+        COM1: "1     ",
+        COM2: "6     ",
+        currentSTPT: "1 ",
+    }
 
 
     const pageBuilder = {
@@ -30,18 +39,25 @@ const DEDscreen = () => {
             let rockerValues = [" ", " ", " "];
             rockerValues[planeData.currentRockerLocation % rockerValues.length] = "↕";
             let templateArray = [
-               [`UHF${rockerValues[0]}${planeData.COM1str}     STPT ${rockerValues[1]} ${planeData.currentSTPT}${planeData.STPTMode}`,
+               [`UHF${rockerValues[0]}${planeDataStr.COM1}     STPT ${rockerValues[1]}${planeDataStr.currentSTPT}${planeDataStr.STPTMode}`,
                 `                        `,
-                `VHF${rockerValues[2]}${planeData.COM2str}      ${planeData.timeStr}`,
+                `VHF${rockerValues[2]}${planeDataStr.COM2}      ${planeDataStr.timeStr}`,
                 `                        `,
-                `${planeData.IFFModeStr}   ${planeData.IFFCode} TIM   ${planeData.NavMode} ${planeData.currentTACAN}`]
+                `${planeDataStr.IFFModeStr}   ${planeData.IFFCode} TIM   ${planeDataStr.NavMode} ${planeDataStr.currentTACAN}`]
               ];
             return templateArray;
             },
-    };
+        
+        list: () => {return [[`        LIST         ${planeDataStr.currentSTPT}↕`,
+                              ` 1DEST 2BNGO 3VIP  RINTG`,
+                              ` 4NAV  5MAN  6INS  EDLNK`,
+                              ` 7EWS  8MODE 9VRP  0MISC`,
+                              `                        `]];
+        }
+    }; // this array stores the template of each DED page and tab and generate the proper strings
 
 
-    const init = () => { // this function is supposed to be called once at the beginning of the page to create the DED
+    const init = () => {
         let DEDcontainer = document.getElementById("DEDcontainer")
         for (i=0; i<5; i++){
             let newRow = document.createElement("div")
@@ -57,9 +73,8 @@ const DEDscreen = () => {
                 addedRow.append(cell)
             }
         }
-        updateDisplayArray(planeData.currentDEDPage)
-        updateDisplayHTML()
-    };
+        refresh()
+    }; // called once at the beginning of the page to create the DED
 
 
     const updateDisplayHTML = () => {
@@ -68,14 +83,11 @@ const DEDscreen = () => {
                 document.getElementById("cell" + i + j).textContent=displayArray[i][j]
             }
         }
-    };
+    }; // updates the HTML page
   
     const updatePlaneData = () => {
         planeData.blink++;
-        d = new Date();
-        timeStr = d.toLocaleTimeString();
-        planeData.timeStr = timeStr;
-    };
+    }; // updates planeData
 
 
     const updateDisplayArray = (page, tab=0) => {
@@ -83,13 +95,14 @@ const DEDscreen = () => {
         let displayArrayBuilder = pageBuilder[selectedPage]();
         displayArray = displayArrayBuilder[tab];
         planeData.currentDEDPage = page;
-    };
+    }; // updates the display array
 
     function refresh(page=planeData.currentDEDPage, tab=planeData.currentDEDtab) {
         updatePlaneData();
+        updatePlaneDataStr();
         updateDisplayArray(page, tab);
         updateDisplayHTML();
-    }
+    } // is supposed to be called every time something changes so that everything is updated nicely
 
     const buttonFunctions = {
         "fourWayUp": function () {
@@ -101,13 +114,111 @@ const DEDscreen = () => {
             refresh();
         },
         "rockerUp": function () {
+            switch (planeData.currentDEDPage) {
+                case "main":
+                    switch(planeData.currentRockerLocation % 3){
+                        case 0:
+                            planeData.COM1++;
+                            break;
+                        case 1:
+                            if (planeData.currentSTPT === 99){
+                                planeData.currentSTPT = 1;
+                            }
+                            else {
+                                planeData.currentSTPT++
+                            }
+                            break;
+                        case 2:
+                            planeData.COM2++;
+                            break;
+                    }
+                    break;
+            }
             refresh();
         },
         "rockerDown": function () {
+            switch (planeData.currentDEDPage) {
+                case "main":
+                    switch(planeData.currentRockerLocation % 3){
+                        case 0:
+                            planeData.COM1--;
+                            break;
+                        case 1:
+                            if (planeData.currentSTPT === 1) {
+                                planeData.currentSTPT = 99
+                            }
+                            else{
+                            planeData.currentSTPT--;}
+                            break;
+                        case 2:
+                            planeData.COM2--;
+                            break;
+                    }
+                    break;
+            }
             refresh();
         },
+        "list": function(){
+            planeData.currentDEDPage = "list";
+            planeData.currentDEDtab = 0;
+        },
+    }; // stores the function of each ICP button
 
-    };
+    function updatePlaneDataStr() {
+        if (planeData.STPTMode % 2 === 0){
+            planeDataStr.STPTMode = " ";
+        }
+        else {
+            planeDataStr.STPTMode = "A";
+        }
+
+        if (planeData.COM1mode === "preset") {
+            let c = (planeData.COM1 % 16) + 1;
+            if (c >= 10) {
+                planeDataStr.COM1 = c + "    ";
+            }
+            else {
+                planeDataStr.COM1 = c + "     "
+            }
+        }
+        else {
+            let comStr = planeData.COM1 + "";
+            while (comStr.length<6){
+                comStr += "0";
+            }
+            planeDataStr.COM1 = comStr;
+            }
+
+        if (planeData.COM2mode === "preset") {
+            let c = (planeData.COM2 % 16) + 1;
+            if (c >= 10) {
+                planeDataStr.COM2 = c + "    ";
+            }
+            else {
+                planeDataStr.COM2 = c + "     "
+            }
+        }
+        else {
+            let comStr = planeData.COM2 + "";
+            while (comStr.length<6){
+                comStr += "0";
+            }
+            planeDataStr.COM2 = comStr;
+            }
+        
+        if (planeData.currentSTPT >= 10){
+            planeDataStr.currentSTPT = planeData.currentSTPT + "";
+        }
+
+        else {
+            planeDataStr.currentSTPT = planeData.currentSTPT + " ";
+        }
+
+
+        d = new Date();
+        timeStr = d.toLocaleTimeString();
+        planeDataStr.timeStr = timeStr;
+    } // updates the strings that will be fed to pageBuilder()
 
     init();
     return {refresh, buttonFunctions};
